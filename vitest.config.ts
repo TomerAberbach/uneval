fe067a40c36305f1a4f4dbbd1e3c138bcff44a71
Tomer Aberbach <tomer@aberba.ch>
2025-11-15 15:19:31 -0500

    chore(ci): update test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     environment: `jsdom`,
     setupFiles: [`vitest.setup.ts`],
-    testTimeout: 30_000,
+    testTimeout: 35_000,
     coverage: {
       include: [`src`],
     },
